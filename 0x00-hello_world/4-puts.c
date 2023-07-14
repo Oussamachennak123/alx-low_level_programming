4729d503e6ecd64ee9166b7dc4ab36c23a649490
oumassa <ousschenaa@gmail.com>
2023-07-13 17:12:31 -0700

    user puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
-/*display*/
-int main () {
+/**
+ * main - print a line using puts fuction
+ * return:0(success)
+ */
+int main (void)
+{
 	puts("Programming is like building a multilingual puzzle");
 	return (0);
 }
