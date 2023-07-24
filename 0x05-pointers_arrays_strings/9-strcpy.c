7f14ae475b0ea5ec8a4d9ab893a5275ef06d1157
oumassa <ousschenaa@gmail.com>
2023-07-24 15:53:15 -0700

    function that copies the string pointed to by src

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -6,12 +6,12 @@
  * @src: copy from
  * Return: string
  */
-char *_strcpy(char *dest, char * src)
+char *_strcpy(char *dest, char *src)
 {
 	int l = 0;
 	int x = 0;
 
-	while (*(src + 1) != '\0')
+	while (*(src + l) != '\0')
 	{
 		l++;
 	}
