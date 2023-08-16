bf7c114ab5ae89e3f460b0f8e0495479d712128a
Oussama <chennak.oussama@gmail.com>
2023-08-17 00:01:46 +0200

    create print name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,35 +1,15 @@
 #include "main.h"
 #include <stdio.h>
 
-void imprimer_nom_majuscules(char *nom) {
-    for (int i = 0; nom[i]; i++) {
-        putchar(toupper(nom[i]));
-    }
-    putchar('\n');
-}
+/**
+ * print_name - print name
+ * @name: name to print
+ * @f: the function to callback
+ */
 
-void imprimer_nom_minuscules(char *nom) {
-    for (int i = 0; nom[i]; i++) {
-        putchar(tolower(nom[i]));
-    }
-    putchar('\n');
-}
-
-void imprimer_nom(char *nom, void (*f)(char *)) {
-    f(nom);
-}
-
-int main() {
-    char nom[] = "Jean Dupont";
-
-    printf("Impression du nom avec imprimer_nom_normal :\n");
-    imprimer_nom(nom, imprimer_nom_normal);
-
-    printf("Impression du nom en majuscules :\n");
-    imprimer_nom(nom, imprimer_nom_majuscules);
-
-    printf("Impression du nom en minuscules :\n");
-    imprimer_nom(nom, imprimer_nom_minuscules);
-
-    return 0;
+void print_name(char *name, void (*f)(char *))
+{
+	if (!name || !f)
+		return;
+	f(name);
 }
